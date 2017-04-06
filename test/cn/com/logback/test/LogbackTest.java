8238d0b95bd0d7204a1f0a804986437ae87ce3e0
fossilrain@org <1024254883@qq.com>
2017-04-06 17:35:09 +0800

    将日志模块由log4j改为logback

---
+++ test/cn/com/logback/test/LogbackTest.java
@@ -0,0 +1,22 @@
+package cn.com.logback.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+
+public class LogbackTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Logger logger = LoggerFactory.getLogger(LogbackTest.class);
+		logger.debug("Hello world.");
+		
+		// print internal state
+		/*LoggerContext lc = (LoggerContext)
+		LoggerFactory.getILoggerFactory();
+		StatusPrinter.print(lc);*/
+	}
+
+}
